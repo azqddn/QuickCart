fb90c0014c7fa92f0ebf6a1442bb202c5fbe537e
azqddn <aziquddin99@gmail.com>
2025-02-25 04:48:51 +0800

    seeder, update, delete in postman

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -16,3 +16,54 @@ export const newProduct = async(req, res) => {
         product
     });
 };
+
+//Get single product details = /api/v1/products/:id
+export const getProductDetails = async(req, res) => {
+    const products = await Product.findById(req.params.id);
+
+    if (!products){
+        return res.status(404).json({
+            error: "Product not found",
+        })
+    };
+
+    res.status(200).json({
+        products,
+    });
+};
+
+//Update product details = /api/v1/products/:id
+export const updateProduct = async(req, res) => {
+    let products = await Product.findById(req.params.id);
+
+    if (!products){
+        res.status(404).json({
+            error: "Product not found",
+        })
+    };
+
+    products = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+
+    res.status(200).json({
+        products,
+    });
+};
+
+//Delete product = /api/v1/products/:id
+export const deleteProduct = async(req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            error: "Product not found",
+        });
+    }
+
+    await product.deleteOne(); // This will now only execute if product exists
+
+    res.status(200).json({
+        message: "Product Deleted",
+    });
+};
